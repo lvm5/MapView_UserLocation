b59bc70b3f3aa86e85ceb1f53d6b7eb5bca71d87
Leandro Vansan de Morais <59117400+lvm5@users.noreply.github.com>
2023-10-27 20:10:13 -0300

    Initial Commit

---
+++ MapView_UserLocation/MapView_UserLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapView_UserLocationApp.swift
+//  MapView_UserLocation
+//
+//  Created by Leandro Morais on 27.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct MapView_UserLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
